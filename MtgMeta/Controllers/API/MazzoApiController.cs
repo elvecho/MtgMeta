977aa1753ec94a6220acf8dbfa61e78be70e8272
Alberto Vecchiatti <albertovecchiatti@gmail.com>
2022-12-05 17:22:42 +0100

    aggiunta api, manca la logica grafica, da aggiungere la relazione tra carta e mazzo

--- MtgMeta/Controllers/API/MazzoApiController.cs
+++ MtgMeta/Controllers/API/MazzoApiController.cs
@@ -5,28 +5,28 @@ using MtgMeta.Models;
 
 namespace MtgMeta.Controllers.API
 {
-    //[Route("api/[controller]/[action]")]
-    //[ApiController]
-    //public class MazzoApiController : ControllerBase
-    //{
-    //    [HttpGet]
-    //    public IActionResult Get(string? search)
-    //    {
-    //        List<Mazzo> Mazzi = new List<Mazzo>();
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class MazzoApiController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get(string? search)
+        {
+            List<Mazzo> Mazzi = new List<Mazzo>();
 
-    //        using (Context db = new Context())
-    //        {
-    //            if(search != null && search != "")
-    //            {
-    //                Mazzi = db.Mazzi.Where(Mazzi => Mazzi.Name.Contains(search) || Mazzi.Carte.ToString().Contains(search)).ToList<Mazzo>();
-    //            }
-    //            else
-    //            {
-    //                Mazzi = db.Mazzi.ToList<Mazzo>();
-    //            }
-    //        }
-    //        return Ok(Mazzi);
-    //    }
+            using (Context db = new Context())
+            {
+                if (search != null && search != "")
+                {
+                    Mazzi = db.Mazzi.Where(Mazzi => Mazzi.Name.Contains(search) || Mazzi.carte.ToString().Contains(search)).ToList<Mazzo>();
+                }
+                else
+                {
+                    Mazzi = db.Mazzi.ToList<Mazzo>();
+                }
+            }
+            return Ok(Mazzi);
+        }
 
-    //}
+    }
 }
